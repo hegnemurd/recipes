233103511f481f6d50592c348fde59275f996764
hegnemurd <hegne.murd@gmail.com>
2022-12-15 22:19:59 +0000

    Continuing working on Routing and passing props

--- project/src/pages/Details.tsx
+++ project/src/pages/Details.tsx
@@ -1,15 +1,35 @@
 import { useParams } from "react-router-dom";
-import { recipes } from "../models/Recipes"; 
+import { recipes } from "../models/Recipes";
 import RecipeDetails from "../components/Details/RecipeDetails";
 
 const Details = () => {
   const params = useParams();
 
+  // function ensure<T>(
+  //   argument: T | undefined | null,
+  //   message: string = "This value was promised to be there."
+  // ): T {
+  //   if (argument === undefined || argument === null) {
+  //     throw new Error(message);
+  //   }
+  //   return argument;
+  // }
+
+  // const chosenRecipe = (query: Recipe) =>
+  //   ensure(recipes.find((chosenRecipe) => chosenRecipe.id === params.recipeId));
+
   const chosenRecipe = recipes.find(
     (chosenRecipe) => chosenRecipe.id === params.recipeId
   );
 
-  return <RecipeDetails recipeName={chosenRecipe?.recipeName} ingredients={chosenRecipe?.ingredients} />;
+  console.log(chosenRecipe);
+
+  return (
+    <RecipeDetails
+      recipeName={chosenRecipe.recipeName}
+      ingredients={chosenRecipe.ingredients}
+    />
+  );
 };
 
 export default Details;
