dc4ed7660d69fa0f98a0904ef801dd06c2726090
hegnemurd <hegne.murd@gmail.com>
2023-01-18 20:29:09 +0000

    Now can edit previous and new recipes

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -6,7 +6,6 @@ import Details from "./pages/Details";
 import { recipes } from "./models/Recipes";
 import NewRecipeForm from "./components/NewRecipeForm/NewRecipeForm";
 import EditRecipeForm from "./components/NewRecipeForm/EditRecipeForm";
-import { useState } from "react";
 
 function App() {
   const navigate = useNavigate();
@@ -34,73 +33,19 @@ function App() {
     editedName: string,
     editedIngr: string[]
   ) => {
-    const replaceRecipe = recipes.find(
-      (replaced) => replaced.id === editedId
-    );
-
-    // debugger
+    const replaceRecipe = recipes.find((replaced) => replaced.id === editedId);
 
     if (replaceRecipe !== undefined) {
-      return console.log("recipe was found")
+      replaceRecipe.img = editedImg;
+      replaceRecipe.recipeName = editedName;
+      replaceRecipe.ingredients = editedIngr;
+      return console.log(replaceRecipe);
     } else {
-      return console.log("recipe not found")
+      console.log("recipe not found");
+      return navigate("/");
     }
-    // ------------------------
-
-    // const index = recipes.findIndex(
-    //   (rec) => rec.id === uniqueId
-    // );
-    // const newRecipes=[...recipes];
-    // newRecipes[index] = uniqueId;
-    // console.log(index);
-
-    // ------------------------
-
-    // const updatedRecipe = (
-    //   uniqueId: string,
-    //   editedImg: string,
-    //   editedName: string,
-    //   editedIngr: string[]
-    // ) => {
-    //   console.log(uniqueId, editedImg, editedName, editedIngr);
-    //   setNewRecipes(
-    //     newRecipes.map((item) => {
-    //       if (item.id === uniqueId) {
-    //         return { ...item, editedImg, editedName, editedIngr };
-    //       } else {
-    //         return item;
-    //       }
-    //     })
-    //   );
-    // };
-
-    // updatedRecipe(uniqueId, editedImg, editedName, editedIngr);
-
-    // ------------------------
-
-    // let filteredData = recipes;
-    // filteredData = recipes.filter((uniqueId) => uniqueId === recipes.id)
-
-    // console.log(newRecipes);
-
-    // ------------------------
-
-    // if (replaceRecipe !== undefined) {
-    //   replaceRecipe.img = editedImg;
-    //   replaceRecipe.recipeName = editedName;
-    //   replaceRecipe.ingredients = editedIngr;
-    // } else {
-    //   navigate("/new-recipe");
-    // }
-
-    // console.log(editedId, editedImg, editedName, editedIngr);
-
-    // console.log("------");
+    
   };
-
-  // console.log(editedRecipeData);
-
-  // console.log(editRecipe.img, enteredImg, enteredName, enteredIngredients);
 
   return (
     <div>
