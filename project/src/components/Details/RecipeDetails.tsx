b32ba6e646d8b38224aca0d3c9c42bc86f66aeff
hegnemurd <hegne.murd@gmail.com>
2023-08-23 18:33:25 +0100

    Revert "Continuing to move over to Material UI"
    This reverts commit cd27ef871c13477cee2f2714e3450a318eccc2c0.

--- project/src/components/Details/RecipeDetails.tsx
+++ project/src/components/Details/RecipeDetails.tsx
@@ -12,92 +12,76 @@ import { Box, Grid } from "@mui/material";
 import { Container } from "react-bootstrap";
 // import classes from "./RecipeDetails.module.css";
 
-const styles = (theme: any) => ({
-  root: {
-    [theme.breakpoints.up("xs")]: {
-      maxWidth: "300",
-    },
-    [theme.breakpoints.up("sm")]: {
-      maxWidth: "500",
-    },
-    [theme.breakpoints.up("lg")]: {
-      maxWidth: "800",
-    },
-    [theme.breakpoints.up("xl")]: {
-      maxWidth: "1000",
-      maxHeight: "700",
-    },
-  },
-});
-
 const RecipeDetails = (props: Recipe) => {
   return (
-    // <Container className={classes.container} fluid="md">
-    //   <Row>
-    //     <Col>
-    //       <img alt="recipe" src={props.img} className={classes.img} />
-    //       <h2>{props.recipeName}</h2>
-    //     </Col>
-    //     <Col className={classes.col}>
-    //       <ul>
-    //         {props.ingredients.map((ingredient: any) => (
-    //           <li key={ingredient} className={classes.li}>
-    //             {ingredient}
-    //           </li>
-    //         ))}
-    //       </ul>
-    //     </Col>
-    //   </Row>
-    // </Container>
+      // <Container className={classes.container} fluid="md">
+      //   <Row>
+      //     <Col>
+      //       <img alt="recipe" src={props.img} className={classes.img} />
+      //       <h2>{props.recipeName}</h2>
+      //     </Col>
+      //     <Col className={classes.col}>
+      //       <ul>
+      //         {props.ingredients.map((ingredient: any) => (
+      //           <li key={ingredient} className={classes.li}>
+      //             {ingredient}
+      //           </li>
+      //         ))}
+      //       </ul>
+      //     </Col>
+      //   </Row>
+      // </Container>
 
 
-    // <Root>
-      <Container>
-        <Grid
-          container
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          style={{ minHeight: "94vh" }}
-        >
-          <Grid item>
-            <Card sx={{ maxHeight: 500, maxWidth: "800", boxShadow: "10" }}>
-              <Box sx={{ display: "flex", flexDirection: "row" }}>
-                <CardMedia
-                  sx={{ height: 500, width: 800 }}
-                  image={props.img}
-                  title={props.recipeName}
-                />
-                <Box sx={{ display: "flex", flexDirection: "column" }}>
-                  <CardContent>
-                    <Typography
-                      id="recName"
-                      gutterBottom
-                      variant="h2"
-                      component="div"
-                    >
-                      {props.recipeName}
-                    </Typography>
-                    <Typography id="ingList" component="div">
-                      <ul>
-                        {props.ingredients.map((ingredient: any) => (
-                          <li key={ingredient}>{ingredient}</li>
-                        ))}
-                      </ul>
-                    </Typography>
-                  </CardContent>
-                  <CardActions
-                    sx={{ justifyContent: "center", alignContent: "end" }}
+    <Container>
+      <Grid
+        container
+        // justifyContent="center"
+        // alignItems="center"
+        // alignSelf="center"
+        // alignContent="center"
+        display="flex"
+        // flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+        style={{ minHeight: "94vh" }}
+      >
+        <Grid item>
+          <Card sx={{  maxHeight: 500, maxWidth: "800", boxShadow: "10" }}>
+            <Box sx={{ display: "flex", flexDirection: "row" }}>
+              <CardMedia
+                sx={{ height: 500, width: 800 }}
+                image={props.img}
+                title={props.recipeName}
+              />
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <CardContent>
+                  <Typography
+                    id="recName"
+                    gutterBottom
+                    variant="h2"
+                    component="div"
                   >
-                    <Button variant="outlined">Outlined</Button>
-                  </CardActions>
-                </Box>
+                    {props.recipeName}
+                  </Typography>
+                  <Typography id="ingList" component="div">
+                    <ul>
+                      {props.ingredients.map((ingredient: any) => (
+                        <li key={ingredient}>{ingredient}</li>
+                      ))}
+                    </ul>
+                  </Typography>
+                </CardContent>
+                <CardActions sx={{justifyContent: "center", alignContent: "end"}}>
+                  <Button size="small">Details</Button>
+                  <Button size="small">Edit</Button>
+                </CardActions>
               </Box>
-            </Card>
-          </Grid>
+            </Box>
+          </Card>
         </Grid>
-      </Container>
-    // </Root>
+      </Grid>
+    </Container>
   );
 };
 
